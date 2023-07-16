5177c8ec83da4d60d267f69d7d2266aaafb2d286
Aarti002 <aartikumarisingh120@gmail.com>
2023-07-16 14:13:07 +0530

    For deployment on netlify

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,21 @@ import { MainContent } from "./MainComponent";
 import { About } from "./components/About";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
-import {Contact} from "./components/Contact";
-import {Experience} from "./components/Experience";
+import { Contact } from "./components/Contact";
+import { Experience } from "./components/Experience";
 
 function App() {
-  
-    return (
-      //const [showFront, setShowFront] = useState(true);
-        <main className="apps text-gray-400 bg-gray-900 body-font">
-          
-        <NavBar />
-        
-        <About />
-        <Projects />
-        <Experience/>
-        <Skills/>
-        <Contact/>
-
-
-
-        </main>
-      
-    );
-  }
+  return (
+    //const [showFront, setShowFront] = useState(true);
+    <main className="apps text-gray-400 bg-gray-900 body-font">
+      <NavBar />
+      <About />
+      <Projects />
+      <Experience />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
 
 export default App;
